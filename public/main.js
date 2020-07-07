01c9a6bf97a609e0fc8f08a3f07a61caa6c940da
João Rangel <joao@hcode.com.br>
2020-07-06 21:08:32 -0300

    init

--- public/main.js
+++ public/main.js
@@ -1,28 +1,18 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js')
-    .then(function(registration) {
-      console.log('Registration successful, scope is:', registration.scope);
-    })
-    .catch(function(error) {
-      console.log('Service worker registration failed, error:', error);
-    });
-  }
- 
-  const body = document.querySelector('body')
-  const btnOpen = document.getElementById('btn-open')
-  const closeMenu = document.querySelectorAll('[data-close="menu"]')
-  const btnFooter = document.querySelector('#footer button')
+const body = document.querySelector('body')
+const btnOpen = document.getElementById('btn-open')
+const closeMenu = document.querySelectorAll('[data-close="menu"]')
+const btnFooter = document.querySelector('#footer button')
 
-  btnFooter.addEventListener('click', e => {
-      window.scrollTo(0, 0)
-  })
-  
-  btnOpen.addEventListener('click', e => {
-      body.classList.add('open-menu')
-  })
-  
-  closeMenu.forEach(el => {
-      el.addEventListener('click', e => {
-          body.classList.remove('open-menu')
-      })
-  })
+btnFooter.addEventListener('click', e => {
+    window.scrollTo(0, 0)
+})
+
+btnOpen.addEventListener('click', e => {
+    body.classList.add('open-menu')
+})
+
+closeMenu.forEach(el => {
+    el.addEventListener('click', e => {
+        body.classList.remove('open-menu')
+    })
+})
