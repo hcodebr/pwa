f9f92d036ca76f833b7975af714fd7f2d6c0c80c
João Rangel <joao@hcode.com.br>
2020-07-21 16:44:11 -0300

    auth

--- public/main.js
+++ public/main.js
@@ -1,3 +1,13 @@
+if('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js')
+    .then(function(registration){
+        console.log('Registration successful, scope is:', registration.scope)
+    })
+    .catch(function(error){
+        console.log('Service Worker registration failed, error:', error)
+    });
+}
+
 const body = document.querySelector('body')
 const btnOpen = document.getElementById('btn-open')
 const closeMenu = document.querySelectorAll('[data-close="menu"]')
